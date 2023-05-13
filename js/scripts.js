23960709b3a265ab354ba0e234f1096b4d6daa6b
Wagner Eduardo de Souza Antunes <duzicoo@gmail.com>
2023-05-12 21:09:13 -0300

    Finalizando

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,10 @@
 function active() {
     const input = document.querySelector('#input');
     const conteudo = input.value;
-    const conteudo_trim = conteudo.trim();
 
-    resultado = conteudo_trim.replace(/\s+/g, "-")
-                             .replace(/ç/g, "c") 
-                             .replace(/â/g, "a") 
-                             .replace(/ã/g, "a") 
-                             .replace(/â/g, "a")
-                             .replace(/á/g, "a")
-                             .replace(/à/g, "a")
-                             .replace(/ê/g, "e")
-                             .replace(/é/g, "e")
-                             .replace(/è/g, "e")
-                             .replace(/î/g, "i")
-                             .replace(/í/g, "i")
-                             .replace(/ì/g, "i")
-                             .replace(/ô/g, "o")
-                             .replace(/ó/g, "o")
-                             .replace(/ò/g, "o")
-                             .replace(/û/g, "u") 
-                             .replace(/ú/g, "u")
-                             .replace(/ù/g, "u")
-                             .replace(/ý/g, "y");
-
-    document.querySelector('#result').value = resultado.toLowerCase();
+    const resultado = conteudo.split('').reverse().join('');
+    document.querySelector('#result').value = resultado;
+    
 }
 
 function copiar() {
